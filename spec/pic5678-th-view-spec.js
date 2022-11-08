ff5e0093f6d4b01b7c0e76b83eaf7bcbf99a9cec
SBOAAAA <fun_pu@hotmail.com>
2022-11-08 14:59:18 +0700

    edit

---
+++ spec/pic5678-th-view-spec.js
@@ -0,0 +1,9 @@
+'use babel';
+
+import Pic5678ThView from '../lib/pic5678-th-view';
+
+describe('Pic5678ThView', () => {
+  it('has one valid test', () => {
+    expect('life').toBe('easy');
+  });
+});
